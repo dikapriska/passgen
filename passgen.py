af1654dcf3a302124d106964967a5eaaf03629dd
dikapriska <dikapriska@gmail.com>
2021-04-14 14:35:21 +0700

    update it

--- passgen.py
+++ passgen.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 import random,string
-characters=string.ascii_lowercase+string.ascii_uppercase+string.digits+"!#-?@*_\~<>{}$%&().+=/"
+characters=string.ascii_lowercase+string.ascii_uppercase+string.digits+"!-?@*_<>{}$%&()+="
 
 password_length = int(input('Enter password length: '))
 password = ''
